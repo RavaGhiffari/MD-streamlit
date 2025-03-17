8dbc9a64560c45a1373f7735c1257a13316fbb2c
Rava <rava.ghiffari@gmail.com>
2025-03-17 14:17:24 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,7 @@ def main():
   st.title('Dermatology Machine Learning')
   st.info('This app uses machine learning')
   #input data by user
-  erythema = st.slider('Erythema', min_value=0, max_value=3, value=2)
+  erythema = st.radio('Erythema', min_value=0, max_value=3, value=2)
 
 if __name__ == "__main__":
   main()
