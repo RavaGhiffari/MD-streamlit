9f35ec0f665a2629ffa846b0885fa0010ea63940
Rava <rava.ghiffari@gmail.com>
2025-03-17 14:15:58 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,6 +4,8 @@ import joblib as jb
 def main():
   st.title('Dermatology Machine Learning')
   st.info('This app uses machine learning')
+  #input data by user
+  erythema = st.slider('Erythema', min_value=0, max_value=3, value=2)
 
 if __name__ == "__main__":
   main()
